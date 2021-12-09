eb531f33b2c8f3c14d64287938b761df1838e6d3
cptq <lim.derek@yahoo.com>
2021-12-09 16:09:58 -0500

    move linkx to top of file

--- models.py
+++ models.py
@@ -8,6 +8,50 @@ import numpy as np
 import scipy.sparse
 from tqdm import tqdm
 
+class LINKX(nn.Module):	
+    """ our LINKX method with skip connections 
+        a = MLP_1(A), x = MLP_2(X), MLP_3(sigma(W_1[a, x] + a + x))
+    """
+
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers, num_nodes, dropout=.5, cache=False, inner_activation=False, inner_dropout=False, init_layers_A=1, init_layers_X=1):
+        super(LINKX, self).__init__()	
+        self.mlpA = MLP(num_nodes, hidden_channels, hidden_channels, init_layers_A, dropout=0)
+        self.mlpX = MLP(in_channels, hidden_channels, hidden_channels, init_layers_X, dropout=0)
+        self.W = nn.Linear(2*hidden_channels, hidden_channels)
+        self.mlp_final = MLP(hidden_channels, hidden_channels, out_channels, num_layers, dropout=dropout)
+        self.in_channels = in_channels
+        self.num_nodes = num_nodes
+        self.A = None
+        self.inner_activation = inner_activation
+        self.inner_dropout = inner_dropout
+
+    def reset_parameters(self):	
+        self.mlpA.reset_parameters()	
+        self.mlpX.reset_parameters()
+        self.W.reset_parameters()
+        self.mlp_final.reset_parameters()	
+
+    def forward(self, data):	
+        m = data.graph['num_nodes']	
+        feat_dim = data.graph['node_feat']	
+        row, col = data.graph['edge_index']	
+        row = row-row.min()
+        A = SparseTensor(row=row, col=col,	
+                 sparse_sizes=(m, self.num_nodes)
+                        ).to_torch_sparse_coo_tensor()
+
+        xA = self.mlpA(A, input_tensor=True)
+        xX = self.mlpX(data.graph['node_feat'], input_tensor=True)
+        x = torch.cat((xA, xX), axis=-1)
+        x = self.W(x)
+        if self.inner_dropout:
+            x = F.dropout(x)
+        if self.inner_activation:
+            x = F.relu(x)
+        x = F.relu(x + xA + xX)
+        x = self.mlp_final(x, input_tensor=True)
+
+        return x
 
 class LINK(nn.Module):
     """ logistic regression on adjacency matrix """
@@ -30,6 +74,42 @@ class LINK(nn.Module):
         elif isinstance(edge_index, SparseTensor):
             A = edge_index.to_torch_sparse_coo_tensor()
         logits = self.W(A)
+        return logits
+
+class LINK_Concat(nn.Module):	
+    """ concate A and X as joint embeddings i.e. MLP([A;X])"""
+
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers, num_nodes, dropout=.5, cache=True):	
+        super(LINK_Concat, self).__init__()	
+        self.mlp = MLP(in_channels + num_nodes, hidden_channels, out_channels, num_layers, dropout=dropout)
+        self.in_channels = in_channels	
+        self.cache = cache
+        self.x = None
+
+    def reset_parameters(self):	
+        self.mlp.reset_parameters()	
+
+    def forward(self, data):	
+        if (not self.cache) or (not isinstance(self.x, torch.Tensor)):
+                N = data.graph['num_nodes']	
+                feat_dim = data.graph['node_feat']	
+                row, col = data.graph['edge_index']	
+                col = col + self.in_channels	
+                feat_nz = data.graph['node_feat'].nonzero(as_tuple=True)	
+                feat_row, feat_col = feat_nz	
+                full_row = torch.cat((feat_row, row))	
+                full_col = torch.cat((feat_col, col))	
+                value = data.graph['node_feat'][feat_nz]	
+                full_value = torch.cat((value, 	
+                                torch.ones(row.shape[0], device=value.device)))	
+                x = SparseTensor(row=full_row, col=full_col,	
+                         sparse_sizes=(N, N+self.in_channels)	
+                            ).to_torch_sparse_coo_tensor()	
+                if self.cache:
+                    self.x = x
+        else:
+                x = self.x
+        logits = self.mlp(x, input_tensor=True)
         return logits
 
 
@@ -650,87 +730,8 @@ class APPNP_Net(nn.Module):
             return x
 
 
-class LINK_Concat(nn.Module):	
-    """ concate A and X as joint embeddings i.e. MLP([A;X])"""
-
-    def __init__(self, in_channels, hidden_channels, out_channels, num_layers, num_nodes, dropout=.5, cache=True):	
-        super(LINK_Concat, self).__init__()	
-        self.mlp = MLP(in_channels + num_nodes, hidden_channels, out_channels, num_layers, dropout=dropout)
-        self.in_channels = in_channels	
-        self.cache = cache
-        self.x = None
-
-    def reset_parameters(self):	
-        self.mlp.reset_parameters()	
-
-    def forward(self, data):	
-        if (not self.cache) or (not isinstance(self.x, torch.Tensor)):
-                N = data.graph['num_nodes']	
-                feat_dim = data.graph['node_feat']	
-                row, col = data.graph['edge_index']	
-                col = col + self.in_channels	
-                feat_nz = data.graph['node_feat'].nonzero(as_tuple=True)	
-                feat_row, feat_col = feat_nz	
-                full_row = torch.cat((feat_row, row))	
-                full_col = torch.cat((feat_col, col))	
-                value = data.graph['node_feat'][feat_nz]	
-                full_value = torch.cat((value, 	
-                                torch.ones(row.shape[0], device=value.device)))	
-                x = SparseTensor(row=full_row, col=full_col,	
-                         sparse_sizes=(N, N+self.in_channels)	
-                            ).to_torch_sparse_coo_tensor()	
-                if self.cache:
-                    self.x = x
-        else:
-                x = self.x
-        logits = self.mlp(x, input_tensor=True)
-        return logits
 
 
-class LINKX(nn.Module):	
-    """ our LINKX method with skip connections 
-        a = MLP_1(A), x = MLP_2(X), MLP_3(sigma(W_1[a, x] + a + x))
-    """
-
-    def __init__(self, in_channels, hidden_channels, out_channels, num_layers, num_nodes, dropout=.5, cache=False, inner_activation=False, inner_dropout=False, init_layers_A=1, init_layers_X=1):
-        super(LINKX, self).__init__()	
-        self.mlpA = MLP(num_nodes, hidden_channels, hidden_channels, init_layers_A, dropout=0)
-        self.mlpX = MLP(in_channels, hidden_channels, hidden_channels, init_layers_X, dropout=0)
-        self.W = nn.Linear(2*hidden_channels, hidden_channels)
-        self.mlp_final = MLP(hidden_channels, hidden_channels, out_channels, num_layers, dropout=dropout)
-        self.in_channels = in_channels
-        self.num_nodes = num_nodes
-        self.A = None
-        self.inner_activation = inner_activation
-        self.inner_dropout = inner_dropout
-
-    def reset_parameters(self):	
-        self.mlpA.reset_parameters()	
-        self.mlpX.reset_parameters()
-        self.W.reset_parameters()
-        self.mlp_final.reset_parameters()	
-
-    def forward(self, data):	
-        m = data.graph['num_nodes']	
-        feat_dim = data.graph['node_feat']	
-        row, col = data.graph['edge_index']	
-        row = row-row.min()
-        A = SparseTensor(row=row, col=col,	
-                 sparse_sizes=(m, self.num_nodes)
-                        ).to_torch_sparse_coo_tensor()
-
-        xA = self.mlpA(A, input_tensor=True)
-        xX = self.mlpX(data.graph['node_feat'], input_tensor=True)
-        x = torch.cat((xA, xX), axis=-1)
-        x = self.W(x)
-        if self.inner_dropout:
-            x = F.dropout(x)
-        if self.inner_activation:
-            x = F.relu(x)
-        x = F.relu(x + xA + xX)
-        x = self.mlp_final(x, input_tensor=True)
-
-        return x
 
 class GPR_prop(MessagePassing):
     '''
