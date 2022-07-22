79c3d4bebcd8e25893f0b41e991d3b4053beb7f8
Xiuyu Li <xl289@cornell.edu>
2022-07-22 02:54:39 -0400

    switch to gdown to download large files

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ pip install ogb==1.2.4
 pip install flake8==3.8.4
 pip install Babel==2.9.0
 pip install flask==1.1.2
+pip install gdown==4.4.0
 
 
 pip install --no-index torch-scatter==2.0.5 -f https://pytorch-geometric.com/whl/torch-1.7.0+$CUDA.html
